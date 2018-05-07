0d0f0425cda39c3ede0e746fff3e71a8949a5faf
yangjing0630 <591813762@qq.com>
2018-05-07 16:44:41 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+use App\Http\Requests\UserRequest;
+
 class UsersController extends Controller
 {
     //TODO:路由中 resource->show() 展示个人信息
@@ -13,6 +15,22 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //TODO:编辑用户信息
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //TODO:更新用户信息
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 
 }
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+//        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email' => 'required|email|unique:users,email,' . Auth::id(),
+            'introduction' => 'max:80',
+        ];
+    }
+
+    /**
+     * 验证中文信息
+     *
+     * */
+
+    public function messages()
+    {
+
+        return [
+            'name.required' => '用户名不能为空',
+            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
+            'name.between' => '用户名必须介于3-25个字符之间',
+            'name.unique' => '用户名已被占用，请重新填写',
+        ];
+
+    }
+}
