0771e578e9468603bdbb989bd5ec508e95a81a33
yangjing0630 <591813762@qq.com>
2018-05-11 13:25:35 +0800

    prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,7 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
         //make_excerpt() 是自定义的辅助方法
         $topic->excerpt = make_excerpt($topic->body);
     }
