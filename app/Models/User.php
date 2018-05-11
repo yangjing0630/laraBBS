6ff09cb81ec94dd1fbfdf681c69de86649736e8e
yangjing0630 <591813762@qq.com>
2018-05-11 18:01:33 +0800

    list user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,5 +39,10 @@ class User extends Authenticatable
         return $this->id == $model->user_id;
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
 
 }
