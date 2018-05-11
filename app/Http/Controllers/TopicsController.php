fc2a64b770e2bd01904058c3bdbf099c936e629a
yangjing0630 <591813762@qq.com>
2018-05-11 15:29:13 +0800

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -23,8 +23,11 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -39,7 +42,8 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-        return redirect()->route('topics.show', $topic->id)->with('success', 'Created successfully.');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
+//        return redirect()->route('topics.show', $topic->id)->with('success', 'Created successfully.');
     }
 
     public function edit(Topic $topic)
@@ -53,8 +57,8 @@ class TopicsController extends Controller
     {
         $this->authorize('update', $topic);
         $topic->update($request->all());
-
-        return redirect()->route('topics.show', $topic->id)->with('success', 'Updated successfully.');
+        return redirect()->to($topic->link())->with('success', '话题更新成功！');
+//        return redirect()->route('topics.show', $topic->id)->with('success', 'Updated successfully.');
     }
 
     public function destroy(Topic $topic)
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Auth::routes();
 //Route::get('/index', 'ChinaBankController@index');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
