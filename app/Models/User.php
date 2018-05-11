df37b546883a1f2191b892e27c23e9eb0ccd8fb2
yangjing0630 <591813762@qq.com>
2018-05-11 13:41:00 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,11 @@ class User extends Authenticatable
         return $this->hasMany(Topic::class);
     }
 
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
+
 }
