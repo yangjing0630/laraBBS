9c0984d2bef3367ae9661ec8b8bafca76ff67605
yangjing0630 <591813762@qq.com>
2018-05-14 18:18:26 +0800

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
                     style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:jisight@focuscv.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{setting('content_email')}}">联系我们</a></p>
     </div>
 </footer>
